50b493b00ebcda4018a1d0caccfdfec49e2bf863
PortfolioCarabelli <federicocarabelli2389@gmail.com>
2023-06-23 14:53:19 -0300

    Cambios generales

--- src/app/books/store/books.action.ts
+++ src/app/books/store/books.action.ts
@@ -2,40 +2,40 @@ import { createAction, props } from '@ngrx/store';
 import { Books } from './books';
 
 export const invokeBooksAPI = createAction(
-  '[Books API] Invoke Books Fetch API'
-);
-
-export const booksFetchAPISuccess = createAction(
-  '[Books API] Fetch API Success',
+  '[API de Libros] Invocar API de Búsqueda de Libros'
+  );
+  
+  export const booksFetchAPISuccess = createAction(
+  '[API de Libros] Éxito en la Búsqueda de Libros',
   props<{ allBooks: Books[] }>()
-);
-
-export const invokeSaveNewBookAPI = createAction(
-  '[Books API] Inovke save new book api',
+  );
+  
+  export const invokeSaveNewBookAPI = createAction(
+  '[API de Libros] Invocar API para Guardar un Nuevo Libro',
   props<{ newBook: Books }>()
-);
-
-export const saveNewBookAPISucess = createAction(
-  '[Books API] save new book api success',
+  );
+  
+  export const saveNewBookAPISucess = createAction(
+  '[API de Libros] Éxito en la API de Guardar un Nuevo Libro',
   props<{ newBook: Books }>()
-);
-
-export const invokeUpdateBookAPI = createAction(
-  '[Books API] Inovke update book api',
+  );
+  
+  export const invokeUpdateBookAPI = createAction(
+  '[API de Libros] Invocar API para Actualizar un Libro',
   props<{ updateBook: Books }>()
-);
-
-export const updateBookAPISucess = createAction(
-  '[Books API] update  book api success',
+  );
+  
+  export const updateBookAPISucess = createAction(
+  '[API de Libros] Éxito en la API de Actualizar un Libro',
   props<{ updateBook: Books }>()
-);
-
-export const invokeDeleteBookAPI = createAction(
-  '[Books API] Inovke delete book api',
+  );
+  
+  export const invokeDeleteBookAPI = createAction(
+  '[API de Libros] Invocar API para Eliminar un Libro',
   props<{id:number}>()
-);
-
-export const deleteBookAPISuccess = createAction(
-  '[Books API] deleted book api success',
+  );
+  
+  export const deleteBookAPISuccess = createAction(
+  '[API de Libros] Éxito en la API de Eliminar un Libro',
   props<{id:number}>()
-);
+  );
